e3d0c88dc6d0edf69ceb0cea6bc1f066bce3a289
Batuhan Kayaoglu <batuhan.kayaoglu@apptotech.com>
2025-02-14 02:11:00 +0300

    supabaseConfig.js removed git

--- supabase/supabaseConfig.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_CONFIG = {
-    SUPABASE_URL: "https://mtebkyuhdjhtrauhflwm.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10ZWJreXVoZGpodHJhdWhmbHdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMDc0OTQsImV4cCI6MjA1NDY4MzQ5NH0.Gh3YoCBrvDhfH4iIqq9XOcqq3sVl-x9WajMqLqAije0",
-    SUPABASE_JWT_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10ZWJreXVoZGpodHJhdWhmbHdtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczOTEwNzQ5NCwiZXhwIjoyMDU0NjgzNDk0fQ.I0-CJWPvrWqoQ8vLmQYnO4YmbSIlauNyOY8t9T_fwXU",
-  };
-  
-  export default SUPABASE_CONFIG;
-  
